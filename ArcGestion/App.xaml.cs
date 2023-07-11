9fd9091a7616a9a4a2bb591ffde597bf0eb97ef5
TSIAZE KENFACK <tsiazezephirin@gmail.com>
2023-07-11 10:37:10 +0100

    Ajoutez des fichiers projet.

---
+++ ArcGestion/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ArcGestion;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ArcGestion/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ArcGestion;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
